f0b280a086bb7187e354e4c53a684427da04f376
marthaberner <marthaberner.mb@gmail.com>
2015-09-10 15:28:37 -0600

    restore heroku api urls

--- js/services/users_service.js
+++ js/services/users_service.js
@@ -2,23 +2,23 @@ app.factory('UsersService', function($http, $cookies) {
   var users;
   return {
     create: function(attrs) {
-      return $http.post('http://localhost:3000/api/signup', attrs).then(function (response) {
+      return $http.post('https://person-project.herokuapp.com/api/signup', attrs).then(function (response) {
         return response.data;
       });
     },
     all: function () {
-      return $http.get('http://localhost:3000/api/users').then(function (response) {
+      return $http.get('https://person-project.herokuapp.com/api/users').then(function (response) {
         users = response.data
         return users;
       })
     },
     signin: function (user) {
-      return $http.post('http://localhost:3000/api/signin', user ).then(function (response) {
+      return $http.post('https://person-project.herokuapp.com/api/signin', user ).then(function (response) {
         return response.data;
       });
     },
     createAdmin: function (admin) {
-      return $http.post('http://localhost:3000/api/admin/new', admin).then(function (response) {
+      return $http.post('https://person-project.herokuapp.com/api/admin/new', admin).then(function (response) {
         return response.data;
       });
     }
