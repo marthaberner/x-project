abfc6a105e5449a872d53b9f0d7af04c882b676d
marthaberner <marthaberner.mb@gmail.com>
2015-10-01 15:30:06 -0600

    update api routes

--- js/services/users_service.js
+++ js/services/users_service.js
@@ -2,33 +2,33 @@ app.factory('UsersService', function($http, $cookies) {
   var users;
   return {
     create: function(attrs) {
-      return $http.post('http://localhost:3000/api/users/signup', attrs).then(function (response) {
+      return $http.post('http://localhost:3000/api/v1/users/signup', attrs).then(function (response) {
         return response.data;
       });
     },
     all: function () {
-      return $http.get('http://localhost:3000/api/users').then(function (response) {
+      return $http.get('http://localhost:3000/api/v1/users').then(function (response) {
        return response.data
       })
     },
     signin: function (user) {
-      return $http.post('http://localhost:3000/api/users', user ).then(function (response) {
+      return $http.post('http://localhost:3000/api/v1/users', user ).then(function (response) {
         return response.data;
       });
     },
     createAdmin: function (admin) {
-      return $http.post('hhttp://localhost:3000/api/users/admin/new', admin).then(function (response) {
+      return $http.post('hhttp://localhost:3000/api/v1/users', admin).then(function (response) {
         return response.data;
       });
     },
     find: function(user){
-      return $http.get('http://localhost:3000/api/users/' + user.id).then(function (response) {
+      return $http.get('http://localhost:3000/api/v1/users/' + user.id).then(function (response) {
         return response
       })
     },
 
     destroy: function (user) {
-      return $http.delete('http://localhost:3000/api/users/' + user).then(function (response) {
+      return $http.delete('http://localhost:3000/api/v1/users/' + user).then(function (response) {
         return response.status === 200 ? true : false;
       })
     }
