187fa0a455e6ed2937093c388aa74ba78d5c6395
marthaberner <marthaberner.mb@gmail.com>
2015-10-07 10:22:59 -0600

    update api calls from local to heroku

--- js/services/surveys_service.js
+++ js/services/surveys_service.js
@@ -1,19 +1,19 @@
 app.factory('SurveysService', function ($http) {
   return {
     all: function () {
-      return $http.get('http://localhost:3000/api/v1/surveys').then(function (response) {
+      return $http.get('https://person-project.herokuapp.com/api/v1/surveys').then(function (response) {
         return response.data;
       });
     },
 
     create: function (survey) {
-      return $http.post('http://localhost:3000/api/v1/surveys', survey).then(function (response) {
+      return $http.post('https://person-project.herokuapp.com/api/v1/surveys', survey).then(function (response) {
         return response.data;
       });
     },
 
     find: function (survey_id) {
-      return $http.get('http://localhost:3000/api/v1/surveys/' + survey_id).then(function (response) {
+      return $http.get('https://person-project.herokuapp.com/api/v1/surveys/' + survey_id).then(function (response) {
         return response.data;
       })
     }
