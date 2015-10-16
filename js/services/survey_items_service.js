2d4164a0ef231e929d15ee0dcd7a5c73304f4abb
marthaberner <marthaberner.mb@gmail.com>
2015-10-16 15:23:24 -0600

    user can take a free form survey with text inputs

--- js/services/survey_items_service.js
+++ js/services/survey_items_service.js
@@ -1,7 +1,8 @@
 app.factory('SurveyItemsService', function ($http) {
+  var url = 'https://person-project.herokuapp.com/api/v1';
   return {
     find: function(id){
-      return $http.get('https://person-project.herokuapp.com/api/v1/survey-items/' + id)
+      return $http.get(url + '/survey-items/' + id)
       .then(function (response) {
         return response.data;
       })
@@ -9,6 +10,10 @@ app.factory('SurveyItemsService', function ($http) {
     getScore: function (results) {
       var answers = results.map(function (num) {
         return Number(num);
+      })
+      answers = answers.filter(function (answer) {
+        var nan = isNaN(answer);
+        return !nan;
       })
       return answers.reduce(function (sum, num) {
         return sum + num;
--- js/services/surveys_service.js
+++ js/services/surveys_service.js
@@ -1,19 +1,20 @@
 app.factory('SurveysService', function ($http) {
+  var url = 'https://person-project.herokuapp.com/api/v1';
   return {
     all: function () {
-      return $http.get('https://person-project.herokuapp.com/api/v1/surveys').then(function (response) {
+      return $http.get(url + '/surveys').then(function (response) {
         return response.data;
       });
     },
 
     create: function (survey) {
-      return $http.post('https://person-project.herokuapp.com/api/v1/surveys', survey).then(function (response) {
+      return $http.post(url + '/surveys', survey).then(function (response) {
         return response.data;
       });
     },
 
     find: function (survey_id) {
-      return $http.get('https://person-project.herokuapp.com/api/v1/surveys/' + survey_id).then(function (response) {
+      return $http.get(url + '/surveys/' + survey_id).then(function (response) {
         return response.data;
       })
     }
