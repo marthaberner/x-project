db46b37c296fd077448c55905671af334c8d6ef2
marthaberner <marthaberner.mb@gmail.com>
2015-10-15 22:05:40 -0600

    user can take a multiple layout choice survey

--- js/controllers/survey_item_controller.js
+++ js/controllers/survey_item_controller.js
@@ -2,9 +2,14 @@ app.controller('SurveyItemController', function ($scope, $state, $location, Surv
   $scope.answers = [];
   $scope.score = $stateParams.score;
   SurveyItemsService.find($stateParams.survey_id).then(function (response) {
-    $scope.surveyItem = response;
-    $scope.surveyOptions = response.options;
-    $scope.subQuestions = response.sub_questions;
+    if(response.length > 1) {
+      $scope.surveyItems = response;
+      $scope.multiChoice = true;
+    } else {
+      $scope.surveyOptions = response[0].options;
+      $scope.subQuestions = response[0].sub_questions;
+      $scope.table = true;
+    }
   })
 
   $scope.submitSurvey = function () {
